2496097c56e7dcd94aec7b27e6ff029e3502717f
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-12-11 16:44:02 -0600

    order page change with cards

--- project/static/js/order.js
+++ project/static/js/order.js
@@ -11,23 +11,36 @@ var span = document.getElementsByClassName("close")[0];
 function open_modal(id) {
   modal.style.display = "block";
   nav.style.opacity = "0.5";
+  console.log(id)
+  // modal for all-american burger
+  if (id === 'AA-burger') {
+    $("#order-title").text("Customize Your Own Burger!");
+    $("#food-image").attr("src", "static/images/all-american-burger2.jpg");
+    $("#patty > label").text("Patties");
+  }
+  else if (id === 'chicken-clucker') {
+    $("#order-title").text("Customize Your Own Chicken Sandwich!");
+    $("#food-image").attr("src", "static/images/chicken-clucker.png");
+    $("#patty > label").text("Chicken");
+  }
 }
 
 // When the user clicks the button, open the modal
 for (i = 0; i < cards.length; i++) {
-  var id = cards[i].id
+  let id = cards[i].id
   cards[i].onclick = () => { open_modal(id) };
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+function close() {
     modal.style.display = "none";
     nav.style.opacity = "1";
 }
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = close;
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        close()
     }
 }
--- project/users/form.py
+++ project/users/form.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, SelectField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = IntegerField('Password', validators=[DataRequired()])
+
+class OrderForm(FlaskForm):
+    patty = SelectField('Patty')
